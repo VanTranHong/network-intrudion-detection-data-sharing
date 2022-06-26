c61c2915db9d9c4a3d4d89436f47aa5e26b8da0e
VanTranHong <47159209+VanTranHong@users.noreply.github.com>
2022-06-26 18:52:19 +0700

    Create nfstream_unsup_rubns.sh

---
+++ CODE/nfstream_unsup_rubns.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+
+# Monday Friday
+
+# python sup_unsup_run.py --day_names Monday Friday --num_attacks 0 3 --attack_types Bot PortScan DDoS --model_name GMM --num_agents=1  --data_rep=nfstream
+
+# python sup_unsup_run.py --day_names Monday Friday --num_attacks 0 3 --attack_types Bot PortScan DDoS --model_name IF --num_agents=1  --data_rep=nfstream &
+
+# python sup_unsup_run.py --day_names Monday Friday --num_attacks 0 3 --attack_types Bot PortScan DDoS --model_name KDE --num_agents=1  --data_rep=nfstream &
+
+# python sup_unsup_run.py --day_names Monday Friday --num_attacks 0 3 --attack_types Bot PortScan DDoS --model_name OCSVM --num_agents=1  --data_rep=nfstream &
+
+# python sup_unsup_run.py --day_names Monday Friday --num_attacks 0 3 --attack_types Bot PortScan DDoS --model_name PCA --num_agents=1  --data_rep=nfstream &
+
+# python sup_unsup_run.py --day_names Monday Friday --num_attacks 0 3 --attack_types Bot PortScan DDoS --model_name GMM --num_agents=10  --data_rep=nfstream &
+
+# python sup_unsup_run.py --day_names Monday Friday --num_attacks 0 3 --attack_types Bot PortScan DDoS --model_name IF --num_agents=10  --data_rep=nfstream &
+
+# python sup_unsup_run.py --day_names Monday Friday --num_attacks 0 3 --attack_types Bot PortScan DDoS --model_name KDE --num_agents=10  --data_rep=nfstream &
+
+# python sup_unsup_run.py --day_names Monday Friday --num_attacks 0 3 --attack_types Bot PortScan DDoS --model_name OCSVM --num_agents=10  --data_rep=nfstream &
+
+# python sup_unsup_run.py --day_names Monday Friday --num_attacks 0 3 --attack_types Bot PortScan DDoS --model_name PCA --num_agents=10  --data_rep=nfstream &
+
+# python sup_unsup_run.py --day_names Monday Friday --num_attacks 0 3 --attack_types Bot PortScan DDoS --model_name GMM --num_agents=1000  --data_rep=nfstream &&
+
+# python sup_unsup_run.py --day_names Monday Friday --num_attacks 0 3 --attack_types Bot PortScan DDoS --model_name IF --num_agents=1000  --data_rep=nfstream &&
+
+# python sup_unsup_run.py --day_names Monday Friday --num_attacks 0 3 --attack_types Bot PortScan DDoS --model_name KDE --num_agents=1000  --data_rep=nfstream  &&
+
+# python sup_unsup_run.py --day_names Monday Friday --num_attacks 0 3 --attack_types Bot PortScan DDoS --model_name OCSVM --num_agents=1000  --data_rep=nfstream &&
+
+# python sup_unsup_run.py --day_names Monday Friday --num_attacks 0 3 --attack_types Bot PortScan DDoS --model_name PCA --num_agents=1000  --data_rep=nfstream
+
+# Monday Wednesday
+
+python sup_unsup_run.py --day_names Monday Wednesday --num_attacks 0 1 --attack_types DoS --model_name GMM --num_agents=1  --data_rep=nfstream
+
+python sup_unsup_run.py --day_names Monday Wednesday --num_attacks 0 1 --attack_types DoS --model_name IF --num_agents=1  --data_rep=nfstream &
+
+python sup_unsup_run.py --day_names Monday Wednesday --num_attacks 0 1 --attack_types DoS --model_name KDE --num_agents=1  --data_rep=nfstream &
+
+python sup_unsup_run.py --day_names Monday Wednesday --num_attacks 0 1 --attack_types DoS --model_name OCSVM --num_agents=1  --data_rep=nfstream &
+
+python sup_unsup_run.py --day_names Monday Wednesday --num_attacks 0 1 --attack_types DoS --model_name PCA --num_agents=1  --data_rep=nfstream &
+
+python sup_unsup_run.py --day_names Monday Wednesday --num_attacks 0 1 --attack_types DoS --model_name GMM --num_agents=10  --data_rep=nfstream &
+
+python sup_unsup_run.py --day_names Monday Wednesday --num_attacks 0 1 --attack_types DoS --model_name IF --num_agents=10  --data_rep=nfstream &
+
+python sup_unsup_run.py --day_names Monday Wednesday --num_attacks 0 1 --attack_types DoS --model_name KDE --num_agents=10  --data_rep=nfstream &
+
+python sup_unsup_run.py --day_names Monday Wednesday --num_attacks 0 1 --attack_types DoS --model_name OCSVM --num_agents=10  --data_rep=nfstream &
+
+python sup_unsup_run.py --day_names Monday Wednesday --num_attacks 0 1 --attack_types DoS --model_name PCA --num_agents=10  --data_rep=nfstream &
+
+python sup_unsup_run.py --day_names Monday Wednesday --num_attacks 0 1 --attack_types DoS --model_name GMM --num_agents=1000  --data_rep=nfstream &&
+
+python sup_unsup_run.py --day_names Monday Wednesday --num_attacks 0 1 --attack_types DoS --model_name IF --num_agents=1000  --data_rep=nfstream &&
+
+python sup_unsup_run.py --day_names Monday Wednesday --num_attacks 0 1 --attack_types DoS --model_name KDE --num_agents=1000  --data_rep=nfstream  &&
+
+python sup_unsup_run.py --day_names Monday Wednesday --num_attacks 0 1 --attack_types DoS --model_name OCSVM --num_agents=1000  --data_rep=nfstream &&
+
+python sup_unsup_run.py --day_names Monday Wednesday --num_attacks 0 1 --attack_types DoS --model_name PCA --num_agents=1000  --data_rep=nfstream
